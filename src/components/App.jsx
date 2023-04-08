fcc39db25c56845dfe95f39400280955f3880981
Isra Zahid <zahidi1@mcmaster.ca>
2023-04-08 19:47:25 -0400

    Refactoring and reformatting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  
-  const [listOfItems, setListOfItems] = useState([]);
+  const [notes, setNotes] = useState([]);
 
-  function handleAdd(item) {
-    setListOfItems(prev => { 
-      return [...prev, item]
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
     });
   }
 
-  function handleDelete(id) {
-    setListOfItems(prev => {
-      return prev.filter((value, index) => {
-          return index !== id
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
       });
-    })
+    });
   }
 
   return (
     <div>
       <Header />
-      <CreateArea onAdd={handleAdd}/>
-      {
-        listOfItems.map((item, index) => <Note key={index} id={index} title={item.title} content={item.content} onDelete={handleDelete}/>)
-      }
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
